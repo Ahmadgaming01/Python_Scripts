2c652cee757a4cfcbcb731363a8af01727eee7ec
ahmad <aalmadani111@gmail.com>
2023-10-06 22:02:16 +0200

    add method to create new folder and add the file name to the folder

--- sort_repo.py
+++ sort_repo.py
@@ -4,8 +4,22 @@ import shutil
 
 datei_pfad = input("Geben Sie der Pfad : ") 
 
-pfad = os.path.dirname(os.path.join( datei_pfad ))
+#pfad = os.path.dirname(os.path.join( datei_pfad ))
 
 
-for pfadname in os.listdir(pfad):
-    print(pfadname)
+for pfadname in os.listdir(datei_pfad):
+    
+
+    if pfadname.endswith(('.png' , '.jpg','.gif','.jpeg')):
+        os.mkdir('images')
+        shutil.copy(pfadname , 'images')
+        os.remove(pfadname)
+        print('Fertig erstellt')
+
+    if pfadname.endswith(('.exe' )):
+        if not os.path.exists('Applications'):
+            os.mkdir('Applications')
+        shutil.copy(pfadname , 'Applications')
+        os.remove(pfadname)
+        print('Fertig erstellt')
+
